b8d1a403e538868f5496a307ae40b8a055867dd8
rlaustria <79557596+rlaustria@users.noreply.github.com>
2024-07-11 18:21:06 +0800

    url fix

--- src/app/lib/service.tsx
+++ src/app/lib/service.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { ApiResponse, User } from '../types';
 
-const url = 'https:/reqres.in';
+const url = 'https://reqres.in';
 
 export function getUsers(page: number) {
     return axios.get<ApiResponse>(`${url}/api/users?page=${page}`);
